18f8f941db8743a9384c8890288a36b0ab7f3574
Jcp2203 <59124356+Jcp22034@users.noreply.github.com>
2022-12-10 12:11:07 +0000

    Fixed

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def fakingtoken():
     #get first part of token
     global id1
     id1 = input("\n\nEnter target's ID: ")
-    if len(id1) != 18 and len(id1) != 19:
+    if len(id1) < 10 and id1.isdigit():
         fakingtokenfailed()
     else:
         if id1.isdigit():
